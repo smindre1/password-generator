042c7ecbdb2db81b0adf848bfeda2cb997fb24b4
Shane Mindreau <smindre1@binghamton.edu>
2023-08-10 16:53:52 -0400

    initial code

---
+++ Develop/script.js
@@ -0,0 +1,75 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+// function writePassword() {
+//   // var password = generatePassword();
+//   alert("Test");
+//   // var passwordText = document.querySelector("#password");
+
+//   // passwordText.value = password;
+
+//   //prompts:
+//   //What is the length of the password? (008-128 char)
+//   //Would you like to include lowercase letters?
+//   //Would you like to include uppercase letters?
+//   //Would you like to include numbers?
+//   //Lastly, would you like to include special characters?
+//   //Make the password using random number generator and arrays.
+//   //Display the password.
+// }
+
+// function generatePassword() {
+//   window.alert("Testing2");
+//   parameters();
+// }
+
+// function parameters() {
+//   var pasLength = 0;
+//   var lowercase = false;
+//   var uppercae = false;
+//   var numbers = false;
+//   var special = false;
+
+//   let choice = prompt("What is the length of the password? (008-128 char)", "");
+//   // if (choice == "yes") {
+//   //   lowercase = true;
+//   // }
+//   //test
+//   window.alert(choice);
+
+//   let choice = prompt("Would you like to include lowercase letters?", "");
+//   if (choice == "yes") {
+//     lowercase = true;
+//   }
+//   //test
+//   window.alert(lowercase);
+
+//   let choice = prompt("Would you like to include uppercase letters?", "");
+//   if (choice == "yes") {
+//     uppercase = true;
+//   }
+//   //test
+//   window.alert(uppercase);
+
+//   let choice = prompt("Would you like to include numbers?", "");
+//   if (choice == "yes") {
+//     numbers = true;
+//   }
+//   //test
+//   window.alert(numbers);
+
+//   let choice = prompt(
+//     "Lastly, would you like to include special characters?",
+//     ""
+//   );
+//   if (choice == "yes") {
+//     special = true;
+//   }
+//   //test
+//   window.alert(special);
+// }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", alert("hello"));
+//writePassword <--generatePassword
